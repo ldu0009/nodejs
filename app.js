e40dafa46be23d7d710dece1298f5263fd5e54c3
ldu0009 <ldu0009@gmail.com>
2018-07-16 14:44:33 +0900

    Template Engine studing.

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ This code is Main application.
 */
 var exp = require('express'); //Web application을 만들기 위한 모듈
 var app = exp(); //application을 리턴한다.
-app.use(exp.static('public')); //이 디렉토리에 html파일을 저장하면 서버의 reboot없이 페이지를 바꿀 수 있다. 정적
+app.locals.pretty = true; //jade로 렌더링 된 html코드를 보기 pretty하게 만들어 준다.
+app.set('view engine','jade'); //'jade'라는 탬플릿 엔진을 사용한다.
+app.set('views','./views') //두 번째 인자에 맞는 이름의 디렉토리가 생성되있어야 한다. 기본 값은 view이다.
+app.use(exp.static('public')); //이 디렉토리에 html파일을 저장하면 html파일의 수정을 서버의 reboot없이 페이지에 적용 시킬 수 있다. 정적
+app.get('/template',function(req,res){
+  res.render('temp',{time:Date(),_title:'Jade'}); //Tamplate engine을 사용하면 send가 아니라 render를 사용해야 한다.
+                                                  //template라는 경로로 들어온 사용자에게는 temp라는 파일을 렌더링 해준다.
+                                                  //temp파일은 위에서 설정한 /view라는 경로에 있어야 한다.
+                                                  //위에서 설정한 Template engine인 jade의 문법에 맡게 respon한다.
+                                                  //++두번째 인자로 객체를 전달해주면 jade는 그 변수들을 사용할 수 있어진다.
+});
 app.get('/',function(req, res){ //Homepage로 접속하였을때
   res.send('Hello!');
 });
-app.get('/dynamic',function(req, res){ //Homepage로 접속하였을때
+app.get('/dynamic',function(req, res){
   var lis = '';
   var time = Date()
   for(var i=0;i<5;i++){
