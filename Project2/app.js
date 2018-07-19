58851389890964ad0c128858c1486360f80812e3
Woky <ldu0009@gmail.com>
2018-07-19 14:35:45 +0900

    Filesystem study.
    Added functionality the file upload. and renewal the Home page and added button for the back to the home.

--- Project2/app.js
+++ Project2/app.js
@@ -1,5 +1,29 @@
 var exp = require('express');
 var bodyParser = require('body-parser');
+var multer = require('multer');
+var fs = require('fs');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) { //저장될 파일의 이름을 설정할 수 있는 부분0
+    var str = file.originalname;
+    var buff = str.split('.');
+    var tmp = buff[0];
+    var files = fs.readdirSync('uploads/');
+    var flag = 1;
+    for(var i = 0; i<files.length; i++){
+      console.log('('+i+')'+buff+'=='+files[i]);
+      if(files[i]==(buff[0]+'.'+buff[1])){ //같은 이름의 파일이 있으면 순자를 붙여주는 기능을 추가
+        buff[0] = tmp+'('+flag+')';
+        i=-1; //for문이 끝나고 i++ 되기 때문에
+        flag = flag + 1;
+      }
+    }
+    cb(null, (buff[0]+'.'+buff[1]));
+  }
+});
+var upload = multer({ storage: storage }); //multer라는 함수에 옵션을 주는 구문 (이 함수는 upload를 하는 미들웨어라고 선언.)
 var fs = require('fs');
 var app = exp();
 var port = 3000;
@@ -12,7 +36,7 @@ app.set('views','./views');
 app.set('view engine','Jade');
 
 app.get('/',function(req,res){
-  res.send('<h1>Main page</h1>');
+  res.render('main');
 });
 app.get(['/topic','/topic/:id'],function(req,res){
   var id = req.params.id;
@@ -25,19 +49,26 @@ app.get(['/topic','/topic/:id'],function(req,res){
     if(id=='new'){
       res.render('new',{topics:files});
     } else if(id){
-      fs.readFile('data/'+id,'utf8',function(err,data){ //encoding을 하지 않으면 Download가 된다.
+      fs.readFile('data/'+id,function(err,data){ //encoding을 하지 않으면 Download가 된다.
         if(err){
           console.log(err);
           res.status(500).send('Internal Server Error'); //500 서버상에 오류가 있다는 의미
         }
-        res.render('view',{topics:files,title: id, description: data});
+        res.send(data);
       });
     } else{
       res.render('view',{topics:files,title: 'Welcome', description: 'Hello, Javascript for server.'});
     }
   });
 });
+app.get('/upload',function(req,res){
+  res.render('upload_form');
+})
 
+app.post('/upload',upload.single('userfile'),function(req,res){
+  console.log(req.file);
+  res.send('Uploaded: '+req.file.filename);
+})
 app.post('/topic',function(req,res){
   var title = req.body.title;
   var description = req.body.description;
@@ -46,7 +77,7 @@ app.post('/topic',function(req,res){
       console.log(err);
       res.status(500).send('Internal Server Error'); //500 서버상에 오류가 있다는 의미
     }
-    res.redirect('/topic/'+title);
+    res.redirect('Success!');
   });
 });
 
